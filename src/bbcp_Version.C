1f0bfea4745c72502d77cbe1d73725750db285b4
Andrew Hanushevsky <abh@stanford.edu>
2015-01-20 21:15:04 -0800

    Bump minor change level to account for changes.

--- src/bbcp_Version.C
+++ src/bbcp_Version.C
@@ -47,9 +47,9 @@ extern bbcp_Config  bbcp_Config;
   
 bbcp_Version::bbcp_Version()
 {
-Copyright = "(c) 2002-2014 by the Board of Trustees of the Leland Stanford, Jr., University";
+Copyright = "(c) 2002-2015 by the Board of Trustees of the Leland Stanford, Jr., University";
 Author    = "Andrew Hanushevsky";
-Version   = "Version: 14.09.02.00.0";
+Version   = "Version: 14.09.02.00.1";
 //           0123456789
 VData     = Version+9;
 };
