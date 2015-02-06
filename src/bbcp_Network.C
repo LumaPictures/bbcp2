80fcde25dc96eb9461f650f80644c69fb50f227c
Andrew Hanushevsky <abh@stanford.edu>
2015-02-06 07:18:38 -0800

    Take into account humongous network buffers.

--- src/bbcp_Network.C
+++ src/bbcp_Network.C
@@ -105,7 +105,8 @@ bbcp_Network::bbcp_Network()
    if ((netFD = open("/proc/sys/net/core/rmem_max", O_RDONLY)) > -1)
       {if (read(netFD, netBuff, sizeof(netBuff)) > 0)
           {maxRcvBuff = atoi(netBuff);
-           maxRcvBuff = maxRcvBuff*2/8192*8192;
+           if (!(maxRcvBuff & 0x40000000) maxRcvBuff = maxRcvBuff<<1;
+           maxRcvBuff = maxRcvBuff/8192*8192;
           }
        close(netFD);
       }
@@ -115,7 +116,8 @@ bbcp_Network::bbcp_Network()
    if ((netFD = open("/proc/sys/net/core/wmem_max", O_RDONLY)) > -1)
       {if (read(netFD, netBuff, sizeof(netBuff)) > 0)
           {maxSndBuff = atoi(netBuff);
-           maxSndBuff = maxSndBuff*2/8192*8192;
+           if (!(maxSndBuff & 0x40000000) maxSndBuff = maxSndBuff<<1;
+           maxSndBuff = maxSndBuff/8192*8192;
           }
        close(netFD);
       }
